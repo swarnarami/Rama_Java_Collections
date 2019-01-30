7177fbbc0787e4cc4afb029124fd3b71f6830bd0
rama <ramihadoop19@gmail.com>
2019-01-30 16:30:48 +0530

    Second commit

--- src/com/rama/learning/Main.java
+++ src/com/rama/learning/Main.java
@@ -3,6 +3,6 @@ package com.rama.learning;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello rama!, welcome to java world");
     }
 }
